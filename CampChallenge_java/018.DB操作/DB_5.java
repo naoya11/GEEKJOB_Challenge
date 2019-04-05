c54226c82adb22668b82c1536b3ae5c3803e292a
Naoya gonda <>
2019-04-05 15:37:18 +0900

    a

---
+++ CampChallenge_java/018.DB操作/DB_5.java
@@ -0,0 +1,56 @@
+
+import static java.lang.System.out;
+import java.sql.*;
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author naoyagonda
+ */
+public class DB_5 {
+       public static void main(String[] args) throws SQLException {
+           Connection db_con=null;
+
+        try {
+    Class.forName("com.mysql.cj.jdbc.Driver").newInstance();  
+    db_con = DriverManager.getConnection("jdbc:mysql://localhost:8889/test","'root'@'localhost'","");
+    if (db_con==null){
+         out.print(1);
+    }
+    
+        PreparedStatement db_st = null;
+    //中略 
+    db_st = db_con.prepareStatement("DELETE from profiles where profilesID = ?");
+    db_st.setInt(1, 5);
+    ResultSet db_data = null;
+    int d = db_st.executeUpdate();
+      System.out.print("DELETE ");
+      System.out.println(d);
+
+//    while(db_data.next()){
+//        out.println(db_data.getString("profilesID")+" "+db_data.getString("name")+" "+db_data.getString("tel")+" "+db_data.getString("age")+" "+db_data.getString("birthday"));
+//    }
+        db_data.close();
+    db_st.close();
+    db_con.close();
+    if (db_con != null){
+  try {
+    db_con.close();
+  } catch (Exception e_con) {
+      System.out.println(e_con.getMessage());
+  }
+}
+        }catch(Exception e){
+                             System.out.println(e);
+       
+        }
+
+    }
+}
